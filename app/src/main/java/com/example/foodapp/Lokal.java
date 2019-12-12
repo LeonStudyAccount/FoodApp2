7d97f2876ffea7e65e7ed384ece2a2f394432e52
DanielRoe97 <Roetepohl97!>
2019-12-12 14:16:34 +0100

    //habe meine Sachen überarbeitet /* Icons eingefügt
    Neue Einträge für die Lokale (2 neue Klassen)
    
    */

---
+++ app/src/main/java/com/example/foodapp/Lokal.java
@@ -0,0 +1,38 @@
+package com.example.foodapp;
+
+import android.view.Menu;
+
+import java.util.Date;
+import com.google.android.gms.maps.model.LatLng;
+
+public class Lokal {
+
+    private int lokalID;
+    private String name;
+    private String strasse;
+    private int hausnummer;
+    private int lokalart;
+    private int nation;
+    private int spez;
+    private Angebot menu;
+    private Date menustand;
+    private Date eroeffungsdatum;
+    private LatLng koordinaten;
+
+
+    public Lokal(int lokalID, String name, String strasse, int hausnummer, int lokalart, int nation, int spez, Angebot menu, Date menustand, Date eroeffungsdatum, LatLng koordinaten) {
+        this.lokalID = lokalID;
+        this.name = name;
+        this.strasse = strasse;
+        this.hausnummer = hausnummer;
+        this.lokalart = lokalart;
+        this.nation = nation;
+        this.spez = spez;
+        this.menu = menu;
+        this.menustand = menustand;
+        this.eroeffungsdatum = eroeffungsdatum;
+        this.koordinaten = koordinaten;
+
+
+    }
+}
