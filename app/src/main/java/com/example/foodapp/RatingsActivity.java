fdb8579e6f10990d637efe540be56c42d0e8d17d
Pascal Jaron <pascaljaron@yahoo.de>
2019-12-16 01:23:58 +0100

    DONE:
    (1)grobes Layoutprototype für die neue Activity zum Anzeigen der Kommentare
    
    +id würde eingetragen, sowie Textviews und eine Bewertung
    
    - nur grobe Darstellung, Schriftgröße sowie Abstände (Seitenabstände) müssen noch geändert werden
    
    (2) Layoutprototype der Ratings Activity Button Sortieren
    
    +OnClickMethode wurde erstellt
    +neue Klasse(MyFilterDialog), sowie neue Activity (custom_dialog_layout) für das Dialogfenster wurden erstellt, inkl. Kommentare
    
    - hinzufügen der Funktionen für Datum und Alphabetsortierung
    - evtl. Änderungen der Darstellung
    
    _________________________________________________________________________________________________
    
    TODO's:
    (1) grobes Layoutprototype der Ratings Activity
    - nur grobe Darstellung, Schriftgröße sowie Abstände (Seitenabstände) müssen noch geändert werden
    - id muss noch geändert werden
    - Buttons müssen entfernt werden

--- app/src/main/java/com/example/foodapp/RatingsActivity.java
+++ app/src/main/java/com/example/foodapp/RatingsActivity.java
@@ -5,14 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class RatingsActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ratings);
+
     }
+
+/**
+ * Die Methode: "displayFilterDialog" ist für den Sortieren Button,
+ * dieser soll die erstellten Kommentare entweder nach (1) Alphabet oder (2) Datum sortieren.
+ * Es wird ein Dialogfenster aufgerufen, indem ein TextView (Sortieren nach), sowie
+ * zwei Checkboxen (Alphabet & Datum) vorhanden ist.
+ * Die Klasse MyFilterDialog erstellt diese Auswahl
+ *
+ */
+
+
+    public void displayFilterDialog(View view){
+
+        MyFilterDialog myFilterDialog = new MyFilterDialog();//< Ein Objekt der Klasse MyFilterDialog wird erstellt
+
+        myFilterDialog.show(getSupportFragmentManager(),"my_filter_dialog");//< hier wird das eben erstellte Objekt (der Dialog) gezeigt; tag ist für die Fragment Section in der Klasse
+    }
+
+
+
 
     public void mapButtonClicked(View view) {
 
